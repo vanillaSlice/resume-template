39e8169d6bd41bd3512bd22dce7f11c8e70254a4
vanillaSlice <mike_lowe_08@hotmail.co.uk>
2018-01-07 16:51:25 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Resume Template
+
+A clone of [this](https://creativemarket.com/ikonome/686585-Material-Resume-Blue/screenshots/#screenshot1) resume template
+built in plain old HTML and CSS. A working version can be viewed [here](https://vanillaslice.github.io/resume-template/).
---
+++ index.js
@@ -0,0 +1,17 @@
+window.addEventListener('load', function() {
+
+  'use strict';
+
+  var pageBtns = [].slice.call(document.querySelectorAll('input[name="page"]'));
+  var pages = [].slice.call(document.querySelectorAll('.page'));
+
+  // show page depending on which button has been clicked
+  pageBtns.forEach(function(btn) {
+    btn.addEventListener('change', function(e) {
+      pages.forEach(function(page) {
+        page.classList.toggle('hidden', page.id !== e.target.value);
+      });
+    });
+  });
+
+});
